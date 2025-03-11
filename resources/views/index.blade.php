0ec958986cede75e761ccaa9f0fcaefb7b013296
alunoenzoye <enzo.eto.senai404@gmail.com>
2025-03-11 10:24:11 -0300

    Criação do LandingPageController, e roteamento das páginas.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ URL::asset('css/index/index.css') }}">
+    <title>Plantamática</title>
+</head>
+<body>
+    <header>
+        <i>
+        </i>
+        <nav>
+            <ul>
+                <a href="{{ route('user.create') }}">
+                    Cadastrar
+                </a>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <header>
+            <h1>Bem Vindo!</h1>
+            <img src="" alt="">
+        </header>
+        
+    </main>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\LandingPageController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [UserController::class, 'index'])->name('user.index');
+Route::get('/', [LandingPageController::class, 'index'])->name('landing-page.index');
+
+Route::get('/user', [UserController::class, 'index'])->name('user.index');
 Route::get('/create-user', [UserController::class, 'create'])->name('user.create');
 Route::post('/store-user', [UserController::class, 'store'])->name('user-store');
